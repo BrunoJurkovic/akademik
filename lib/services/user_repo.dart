2491da662377f56562d9174e6c301dc36652f9f6
Bruno Jurkovic <bruno.jurkovic1@gmail.com>
2021-01-22 20:09:56 +0100

    AutoRoute

--- lib/services/user_repo.dart
+++ lib/services/user_repo.dart
@@ -1,15 +1,24 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class UserRepository with ChangeNotifier {
   final FirebaseAuth _authInstance = FirebaseAuth.instance;
+  final CollectionReference _firestoreInstance =
+      FirebaseFirestore.instance.collection('users');
 
   Future<void> addUserToFirebase(
       String username, String password, String email, bool isLogin) async {
     if (!isLogin) {
-      await _authInstance.createUserWithEmailAndPassword(
+      final userCredential = await _authInstance.createUserWithEmailAndPassword(
         email: email,
         password: password,
+      );
+      await _firestoreInstance.doc(userCredential.user.uid).set(
+        {
+          'username': username,
+          'email': email,
+        },
       );
     } else {
       await _authInstance.signInWithEmailAndPassword(
