07fdd329c7bf7ba6ce10c04c2c24def89e911925
Bruno Jurkovic <bruno.jurkovic1@gmail.com>
2021-01-23 17:37:30 +0100

    Google fonts integration

--- lib/screens/login/login.dart
+++ lib/screens/login/login.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class AuthScreen extends StatefulWidget {
   @override
@@ -10,6 +11,27 @@ class _AuthScreenState extends State<AuthScreen> {
   Widget build(BuildContext context) {
     final width = MediaQuery.of(context).size.width;
     final height = MediaQuery.of(context).size.height;
-    return Scaffold(body: Column());
+    return Scaffold(
+        body: SafeArea(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          Center(
+            child: Container(
+              width: width * 0.6,
+              height: height * 0.6,
+              child: Image.asset('assets/images/login.png'),
+            ),
+          ),
+          Text(
+            'Login',
+            style: GoogleFonts.montserrat(
+              color: Colors.black87,
+              fontWeight: FontWeight.w700,
+            ),
+          )
+        ],
+      ),
+    ));
   }
 }
