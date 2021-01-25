b0fc11ba36600c4248b4f62a369a6a2983bcef9f
Bruno Jurkovic <bruno.jurkovic1@gmail.com>
2021-01-25 14:46:32 +0100

    Added new route

--- lib/routes/akademik_router.dart
+++ lib/routes/akademik_router.dart
@@ -1,9 +1,11 @@
 import 'package:akademik/screens/home/home.dart';
 import 'package:akademik/screens/login/login.dart';
+import 'package:akademik/screens/menu/menu.dart';
 import 'package:auto_route/auto_route_annotations.dart';
 
 @AdaptiveAutoRouter(routes: <AutoRoute>[
   AdaptiveRoute(page: AuthScreen),
   AdaptiveRoute(page: HomeScreen),
+  AdaptiveRoute(page: MenuScreen),
 ])
 class $AkademikRouter {}
