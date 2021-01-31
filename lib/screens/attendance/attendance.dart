b168a4e27903ae9c6c3f5df7dd4c5167ccedfec5
Bruno Jurkovic <bruno.jurkovic1@gmail.com>
2021-01-31 19:47:54 +0100

    Indicator widget

--- lib/screens/attendance/attendance.dart
+++ lib/screens/attendance/attendance.dart
@@ -1,3 +1,4 @@
+import 'package:akademik/components/title_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
@@ -31,9 +32,62 @@ class _AttendanceScreenState extends State<AttendanceScreen> {
       ),
       body: SingleChildScrollView(
         child: Column(
-          children: [],
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Padding(
+              padding: const EdgeInsets.fromLTRB(10, 20, 0, 0),
+              child: TitleWidget(text: 'Attendance'),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                IndicatorWidget(color: Colors.redAccent, text: 'Unapproved')
+              ],
+            )
+          ],
         ),
       ),
+    );
+  }
+}
+
+class IndicatorWidget extends StatelessWidget {
+  const IndicatorWidget({
+    Key key,
+    @required this.color,
+    @required this.text,
+  }) : super(key: key);
+
+  final Color color;
+  final String text;
+
+  @override
+  Widget build(BuildContext context) {
+    final width = MediaQuery.of(context).size.width;
+    final height = MediaQuery.of(context).size.height;
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.start,
+      children: [
+        Container(
+          height: 20,
+          width: 20,
+          decoration: BoxDecoration(
+            shape: BoxShape.circle,
+            color: color,
+          ),
+        ),
+        SizedBox(
+          width: width * 0.015,
+        ),
+        Text(
+          text,
+          style: GoogleFonts.montserrat(
+            color: Colors.black,
+            fontWeight: FontWeight.w600,
+            fontSize: height * 0.015,
+          ),
+        )
+      ],
     );
   }
 }
