7f6cd3a325990e5e70925ea49e96c6ed179f5634
Bruno Jurkovic <bruno.jurkovic1@gmail.com>
2021-01-31 19:33:33 +0100

    Added attendance repository.

---
+++ lib/services/attendance_repo.dart
@@ -0,0 +1,22 @@
+import 'package:akademik/providers/attendance.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+
+class AttendanceRepository with ChangeNotifier {
+  final CollectionReference _attendanceReference =
+      FirebaseFirestore.instance.collection('attendance');
+  final List<AkademikAttendance> _attendanceList = [];
+
+  Future<void> fetchAttendance(String userId) async {
+    final query =
+        await _attendanceReference.where('userId', isEqualTo: userId).get();
+    query.docs.forEach((doc) {
+      _attendanceList.add(AkademikAttendance.fromDocument(doc));
+    });
+    notifyListeners();
+  }
+
+  List<AkademikAttendance> get getAttendanceList {
+    return _attendanceList;
+  }
+}
