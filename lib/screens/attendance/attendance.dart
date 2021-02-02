972857f38c3ec48d4fb82046478a9171fc634427
Bruno Jurkovic <bruno.jurkovic1@gmail.com>
2021-02-02 10:32:01 +0100

    Attendance screen

--- lib/screens/attendance/attendance.dart
+++ lib/screens/attendance/attendance.dart
@@ -1,6 +1,11 @@
 import 'package:akademik/components/title_widget.dart';
+import 'package:akademik/providers/attendance.dart';
+import 'package:akademik/services/attendance_repo.dart';
+import 'package:akademik/services/user_repo.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:intl/intl.dart';
+import 'package:provider/provider.dart';
 
 class AttendanceScreen extends StatefulWidget {
   @override
@@ -8,6 +13,21 @@ class AttendanceScreen extends StatefulWidget {
 }
 
 class _AttendanceScreenState extends State<AttendanceScreen> {
+  List<AkademikAttendance> attendance = [];
+  @override
+  void initState() {
+    Future.delayed(Duration.zero, () async {
+      await Provider.of<AttendanceRepository>(context, listen: false)
+          .fetchAttendance(Provider.of<UserRepository>(context, listen: false)
+              .currentUser
+              .userId);
+      attendance = Provider.of<AttendanceRepository>(context, listen: false)
+          .getAttendanceList;
+      print(attendance.toString());
+    });
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     final width = MediaQuery.of(context).size.width;
@@ -35,18 +55,119 @@ class _AttendanceScreenState extends State<AttendanceScreen> {
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Padding(
-              padding: const EdgeInsets.fromLTRB(10, 20, 0, 0),
+              padding: const EdgeInsets.fromLTRB(10, 20, 0, 25),
               child: TitleWidget(text: 'Attendance'),
             ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                IndicatorWidget(color: Colors.redAccent, text: 'Unapproved')
-              ],
+            IndicatorDotRow(),
+            SizedBox(
+              height: 25,
+            ),
+            Container(
+              width: width,
+              height: height * 0.7,
+              child: ListView.builder(
+                itemCount: attendance.length,
+                itemBuilder: (ctx, index) {
+                  return Center(
+                    child: Container(
+                      width: width,
+                      height: height * 0.05,
+                      decoration: BoxDecoration(color: Colors.black12),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          Padding(
+                            padding: EdgeInsets.fromLTRB(10, 0, 5, 0),
+                            child: Text(
+                              DateFormat.yMMMd()
+                                  .format(attendance[index].timestamp),
+                              style: GoogleFonts.montserrat(
+                                color: Colors.black,
+                                fontWeight: FontWeight.w500,
+                                fontSize: height * 0.015,
+                              ),
+                            ),
+                          ),
+                          Padding(
+                            padding: EdgeInsets.fromLTRB(10, 0, 5, 0),
+                            child: Text(
+                              '${attendance[index].classTime}',
+                              style: GoogleFonts.montserrat(
+                                color: Colors.black,
+                                fontWeight: FontWeight.w500,
+                                fontSize: height * 0.015,
+                              ),
+                            ),
+                          ),
+                          Padding(
+                            padding: EdgeInsets.fromLTRB(10, 0, 5, 0),
+                            child: Text(
+                              attendance[index].aclass,
+                              style: GoogleFonts.montserrat(
+                                color: Colors.black,
+                                fontWeight: FontWeight.w500,
+                                fontSize: height * 0.015,
+                              ),
+                            ),
+                          ),
+                          Padding(
+                            padding: EdgeInsets.fromLTRB(10, 0, 5, 0),
+                            child: IndicatorWidget(color: Colors.red, text: ''),
+                          ),
+                          Padding(
+                            padding: EdgeInsets.fromLTRB(10, 0, 5, 0),
+                            child: Text(
+                              attendance[index].reason,
+                              style: GoogleFonts.montserrat(
+                                color: Colors.black,
+                                fontWeight: FontWeight.w500,
+                                fontSize: height * 0.015,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  );
+                },
+              ),
             )
           ],
         ),
       ),
+    );
+  }
+}
+
+class IndicatorDotRow extends StatelessWidget {
+  const IndicatorDotRow({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        IndicatorWidget(
+          color: Colors.redAccent,
+          text: 'Unapproved',
+        ),
+        SizedBox(
+          width: 10,
+        ),
+        IndicatorWidget(
+          color: Colors.greenAccent,
+          text: 'Approved',
+        ),
+        SizedBox(
+          width: 10,
+        ),
+        IndicatorWidget(
+          color: Colors.grey,
+          text: 'Waiting',
+        ),
+      ],
     );
   }
 }
@@ -76,9 +197,11 @@ class IndicatorWidget extends StatelessWidget {
             color: color,
           ),
         ),
-        SizedBox(
-          width: width * 0.015,
-        ),
+        text.isNotEmpty
+            ? SizedBox(
+                width: width * 0.015,
+              )
+            : SizedBox(),
         Text(
           text,
           style: GoogleFonts.montserrat(
