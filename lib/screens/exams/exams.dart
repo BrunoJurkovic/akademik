1971ad58b786a434a01405a723dc7afdee68bdf9
Bruno Jurkovic <bruno.jurkovic1@gmail.com>
2021-02-04 14:41:20 +0100

    Display calendar on exams page

--- lib/screens/exams/exams.dart
+++ lib/screens/exams/exams.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:table_calendar/table_calendar.dart';
 
 class ExamsScreen extends StatefulWidget {
   @override
@@ -6,8 +8,48 @@ class ExamsScreen extends StatefulWidget {
 }
 
 class _ExamsScreenState extends State<ExamsScreen> {
+  CalendarController _calendarController;
+
+  @override
+  void initState() {
+    _calendarController = CalendarController();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _calendarController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    final width = MediaQuery.of(context).size.width;
+    final height = MediaQuery.of(context).size.height;
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.deepPurpleAccent.withOpacity(0.85),
+        title: Text(
+          'Exams',
+          style: GoogleFonts.montserrat(
+            color: Colors.white,
+            fontWeight: FontWeight.w600,
+            fontSize: height * 0.025,
+          ),
+        ),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.only(
+            bottomLeft: Radius.circular(20),
+            bottomRight: Radius.circular(20),
+          ),
+        ),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          TableCalendar(calendarController: _calendarController),
+        ],
+      ),
+    );
   }
 }
