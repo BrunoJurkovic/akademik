bff10422a40488db7089b46305b693554a4a4068
Bruno Jurkovic <bruno.jurkovic1@gmail.com>
2021-02-04 20:51:28 +0100

    Argument passing for news item screen

---
+++ lib/screens/news/news_item_screen/news_item_screen.dart
@@ -0,0 +1,21 @@
+import 'package:akademik/providers/news.dart';
+import 'package:auto_route/auto_route.dart';
+
+import 'package:flutter/material.dart';
+
+class NewsItemScreen extends StatefulWidget {
+  final AkademikNews newsItem;
+
+  const NewsItemScreen({Key key, this.newsItem}) : super(key: key);
+
+  @override
+  _NewsItemScreenState createState() => _NewsItemScreenState();
+}
+
+class _NewsItemScreenState extends State<NewsItemScreen> {
+  @override
+  Widget build(BuildContext context) {
+    RouteData.of(context).arguments;
+    return Scaffold();
+  }
+}
