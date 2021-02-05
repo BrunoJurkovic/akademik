b2aeed36f2a1078bd6f7a0281bdceb5d7027d9bb
Bruno Jurkovic <bruno.jurkovic1@gmail.com>
2021-02-05 12:19:31 +0100

    Sharing news

--- lib/screens/news/news_item_screen/news_item_screen.dart
+++ lib/screens/news/news_item_screen/news_item_screen.dart
@@ -1,7 +1,10 @@
 import 'package:akademik/providers/news.dart';
 import 'package:auto_route/auto_route.dart';
+import 'package:cached_network_image/cached_network_image.dart';
 
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:share/share.dart';
 
 class NewsItemScreen extends StatefulWidget {
   final AkademikNews newsItem;
@@ -15,6 +18,103 @@ class NewsItemScreen extends StatefulWidget {
 class _NewsItemScreenState extends State<NewsItemScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    final width = MediaQuery.of(context).size.width;
+    final height = MediaQuery.of(context).size.height;
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.deepPurpleAccent.withOpacity(0.85),
+        title: Text(
+          widget.newsItem.name,
+          style: GoogleFonts.montserrat(
+            color: Colors.white,
+            fontWeight: FontWeight.w600,
+            fontSize: height * 0.025,
+          ),
+        ),
+        // shape: RoundedRectangleBorder(
+        //   borderRadius: BorderRadius.only(
+        //     bottomLeft: Radius.circular(20),
+        //     bottomRight: Radius.circular(20),
+        //   ),
+        // ),
+      ),
+      body: SingleChildScrollView(
+        child: Container(
+          height: height,
+          width: width,
+          child: Stack(
+            children: [
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Container(
+                    width: width,
+                    height: height * 0.3,
+                    child: CachedNetworkImage(
+                      imageUrl: widget.newsItem.imageUrl,
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
+                    child: Text(
+                      'NEWS',
+                      style: GoogleFonts.montserrat(
+                        color: Colors.black,
+                        fontWeight: FontWeight.w600,
+                        fontSize: height * 0.018,
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.symmetric(vertical: 10, horizontal: 15),
+                    child: Text(
+                      widget.newsItem.name,
+                      style: GoogleFonts.montserrat(
+                        color: Colors.black,
+                        fontWeight: FontWeight.w700,
+                        fontSize: height * 0.028,
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.symmetric(vertical: 10, horizontal: 15),
+                    child: Text(
+                      widget.newsItem.description,
+                      style: GoogleFonts.montserrat(
+                        color: Colors.black,
+                        fontWeight: FontWeight.w500,
+                        fontSize: height * 0.02,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              Positioned(
+                top: height * 0.7,
+                left: width * 0.35,
+                child: FlatButton(
+                  color: Colors.deepPurpleAccent.withOpacity(0.8),
+                  onPressed: () {
+                    Share.share('''
+                    ${widget.newsItem.name}
+                    ${widget.newsItem.description}
+                    ''');
+                  },
+                  child: Text(
+                    'SHARE',
+                    style: GoogleFonts.montserrat(
+                      color: Colors.black,
+                      fontWeight: FontWeight.w500,
+                      fontSize: height * 0.02,
+                    ),
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
