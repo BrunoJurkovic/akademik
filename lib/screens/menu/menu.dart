54b86ce5a2e3d7468358fc3ceebeec4801b83030
Bruno Jurkovic <bruno.jurkovic1@gmail.com>
2021-02-09 09:46:03 +0100

    Added grades-screen route

--- lib/screens/menu/menu.dart
+++ lib/screens/menu/menu.dart
@@ -105,7 +105,11 @@ class _MenuScreenState extends State<MenuScreen> {
                     callback: () {
                       ExtendedNavigator.of(context).push('/news-screen');
                     }),
-                AkademikGridItem(text: 'Grades', callback: () {}),
+                AkademikGridItem(
+                    text: 'Grades',
+                    callback: () {
+                      ExtendedNavigator.of(context).push('/grades-screen');
+                    }),
                 AkademikGridItem(text: 'Study', callback: () {}),
                 AkademikGridItem(text: 'Notes', callback: () {}),
                 AkademikGridItem(text: 'Profile', callback: () {}),
