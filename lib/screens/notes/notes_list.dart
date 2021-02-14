396b90d6b1c83d3bde9bb014fe54c326a4a4b8b6
Bruno Jurkovic <bruno.jurkovic1@gmail.com>
2021-02-14 14:45:10 +0100

    Added notes screen appbar

---
+++ lib/screens/notes/notes_list.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class NotesListScreen extends StatefulWidget {
+  @override
+  _NotesListScreenState createState() => _NotesListScreenState();
+}
+
+class _NotesListScreenState extends State<NotesListScreen> {
+  @override
+  Widget build(BuildContext context) {
+    final width = MediaQuery.of(context).size.width;
+    final height = MediaQuery.of(context).size.height;
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.deepPurpleAccent.withOpacity(0.85),
+        title: Text(
+          'Notes',
+          style: GoogleFonts.montserrat(
+            color: Colors.white,
+            fontWeight: FontWeight.w600,
+            fontSize: height * 0.025,
+          ),
+        ),
+      ),
+    );
+  }
+}
