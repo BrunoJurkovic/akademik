a28f51d1377513fd2f9730e6ff48fac2cdc099e1
Bruno Jurkovic <bruno.jurkovic1@gmail.com>
2021-02-16 16:39:27 +0100

    Added basic functions for handling notes

--- lib/providers/notes.dart
+++ lib/providers/notes.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
@@ -6,8 +8,10 @@ class AkademikNote with ChangeNotifier {
   final DateTime dateModified;
   final String userId;
   final String note;
+  final String noteId;
 
-  AkademikNote({this.className, this.dateModified, this.userId, this.note});
+  AkademikNote(
+      {this.className, this.dateModified, this.userId, this.note, this.noteId});
 
   factory AkademikNote.fromDocument(DocumentSnapshot doc) {
     return AkademikNote(
@@ -16,6 +20,17 @@ class AkademikNote with ChangeNotifier {
           DateTime.fromMillisecondsSinceEpoch(doc['dateModified'] as int),
       userId: doc['userId'] as String,
       note: doc['note'] as String,
+      noteId: doc.id,
     );
+  }
+
+  static Map<String, dynamic> noteToMap(AkademikNote note) {
+    return {
+      'className': note.className,
+      'dateModified': note.dateModified.millisecondsSinceEpoch,
+      'userId': note.userId,
+      'note': note.note,
+      'noteId': note.noteId,
+    };
   }
 }
--- lib/services/notes_repo.dart
+++ lib/services/notes_repo.dart
@@ -14,6 +14,22 @@ class NotesRepository with ChangeNotifier {
   Future<void> fetchNotes(String userId) async {
     final query = await reference.where('userId', isEqualTo: userId).get();
 
-    query.docs.forEach((doc) {});
+    query.docs.forEach((doc) {
+      _notes.add(AkademikNote.fromDocument(doc));
+    });
+
+    notifyListeners();
+  }
+
+  Future<void> updateOrCreateNote(AkademikNote note) async {
+    _notes.add(note);
+    await reference.doc(note.noteId).set(AkademikNote.noteToMap(note));
+    notifyListeners();
+  }
+
+  Future<void> deleteNote(AkademikNote note) async {
+    _notes.removeWhere((element) => element.noteId == note.noteId);
+    await reference.doc(note.noteId).delete();
+    notifyListeners();
   }
 }
