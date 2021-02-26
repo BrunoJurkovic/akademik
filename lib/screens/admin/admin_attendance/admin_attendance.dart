49841d9f81ad5b09941022a82a1c7c44134c7b52
Bruno Jurkovic <bruno.jurkovic1@gmail.com>
2021-02-26 18:25:28 +0100

    Boilerplate for attendance screen.

---
+++ lib/screens/admin/admin_attendance/admin_attendance.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class AdminAttendanceScreen extends StatefulWidget {
+  @override
+  _AdminAttendanceScreenState createState() => _AdminAttendanceScreenState();
+}
+
+class _AdminAttendanceScreenState extends State<AdminAttendanceScreen> {
+  @override
+  Widget build(BuildContext context) {
+    final width = MediaQuery.of(context).size.width;
+    final height = MediaQuery.of(context).size.height;
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.deepPurpleAccent.withOpacity(0.85),
+        title: Text(
+          'Edit Homework',
+          style: GoogleFonts.montserrat(
+            color: Colors.white,
+            fontWeight: FontWeight.w600,
+            fontSize: height * 0.025,
+          ),
+        ),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.only(
+            bottomLeft: Radius.circular(20),
+            bottomRight: Radius.circular(20),
+          ),
+        ),
+      ),
+    );
+  }
+}
